eca9f1d2cd71e72a3f4136fa6163e129e8443467
RaineHanley <rainehanley@gmail.com>
2023-05-26 15:33:35 -0700

    added all controllers, and used DRY

---
+++ app/controllers/pets_controller.rb
@@ -0,0 +1,53 @@
+class PetsController < ApplicationController
+  class PhotosController < ApplicationController
+    def index
+      @pets = Pet.all
+    end
+  end
+
+  def show
+    @pet = Pet.find_by(id: params[:id])
+  end
+
+  def new
+    @pet = Pet.new
+  end
+
+  def create
+    @pet = Pet.create(pet_params)
+    if @pet.save
+      redirect_to @pet
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
+end
+
+def edit
+  @pet = Pet.find_by(id: params[:id])
+  render :edit
+end
+
+def update
+  @pet = Pet.find_by(id: params[:id])
+  @pet.update(
+    breed: params[:breed],
+    color: params[:color],
+    size: params[:size],
+    age: params[:age],
+    img_url: params[:img_url],
+  )
+  redirect_to "/pets"
+end
+
+def destroy
+  @pet = Pet.find_by(id: params[:id])
+  @pet.destroy
+  redirect_to "/pets", status: :see_other
+end
